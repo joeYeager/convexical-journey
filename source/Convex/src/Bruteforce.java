cb4b86e9074edcff901158dab9cf88a4c652e62d
Drew Gohman <dagohman@gmail.com>
2015-03-03 14:20:09 -0800

    updated Bruteforce to include if the points are overlapping

--- source/Convex/src/Bruteforce.java
+++ source/Convex/src/Bruteforce.java
@@ -18,16 +18,21 @@ public class Bruteforce extends Algorithm {
 					if(i != e) {
 						Point E = list.get(e);				//Temp Point E
 						int changes = 0, state = Integer.MIN_VALUE;
-						for(int z = 0; z < list.size(); ++z) {	//All the points for Z
-							if(z != i && z != e) {
-								Point Z = list.get(z);			//Temp point for Z
-								int loc = Location(I, E, Z);
-								if(state != loc) {
-									state = loc;
-									changes += 1;
+						if(I.x != E.x && I.y != E.y) {
+							for(int z = 0; z < list.size(); ++z) {	//All the points for Z
+								if(z != i && z != e ) {
+									Point Z = list.get(z);			//Temp point for Z
+									if(Z.x != I.x && Z.y != I.y &&
+										Z.x != E.x && Z.y != I.y) {
+										int loc = Location(I, E, Z);
+										if(state != loc) {
+											state = loc;
+											changes += 1;
+										}
+									}	
 								}
-							}
 								
+							}	
 						}
 						if(changes == 1) {
 							hull.add(I);
